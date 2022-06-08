7c0e3f139156a8fc8f4660f8acd336912ac677c3
Jacob Ian Matthews <jacob@jacobian.com.au>
2022-06-08 13:53:34 +1000

    Update provider

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,11 +1,11 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import AuthProvider from "../context/AuthProvider/AuthProvider";
+import AuthProvider from "../context/auth/AuthProvider";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </AuthProvider>
   );
 }
