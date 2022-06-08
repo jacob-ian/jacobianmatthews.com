09072994e73ae1b63feb77e7bb24ff86ed901324
Jacob Ian Matthews <jacob@jacobian.com.au>
2022-06-08 14:00:08 +1000

    Add test for useAuthService

---
+++ frontend/hooks/useAuthService.test.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from "@testing-library/react";
+import { AuthServiceContext } from "../context/auth/AuthServiceContext";
+import { AuthService } from "../services/auth/AuthService";
+import { useAuthService } from "./useAuthService";
+
+function TestUseAuthService() {
+  const auth = useAuthService();
+  return <div data-testid="auth">{!!auth ? "true" : "false"}</div>;
+}
+
+describe("useAuthService hook", () => {
+  it("Should render 'false' if AuthService.Provider has a null value", async () => {
+    render(
+      <AuthServiceContext.Provider value={null}>
+        <TestUseAuthService />
+      </AuthServiceContext.Provider>,
+    );
+
+    const renderedHookResult = await screen.findByTestId("auth");
+    expect(renderedHookResult.innerHTML).toEqual("false");
+  });
+
+  it("Should render 'true' if AuthService.Provider has the value of an AuthService instance", async () => {
+    const fakeService: any = { test: "hello" };
+    render(
+      <AuthServiceContext.Provider value={fakeService}>
+        <TestUseAuthService />
+      </AuthServiceContext.Provider>,
+    );
+
+    const renderedHookResult = await screen.findByTestId("auth");
+    expect(renderedHookResult.innerHTML).toEqual("true");
+  });
+});
