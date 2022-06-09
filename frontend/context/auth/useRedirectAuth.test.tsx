e836340220bd0a4f207cc1a0fcc793fa57065df7
Jacob Ian Matthews <jacob@jacobian.com.au>
2022-06-09 11:17:28 +1000

    Add tests to useRedirectAuth

--- frontend/context/auth/useRedirectAuth.test.tsx
+++ frontend/context/auth/useRedirectAuth.test.tsx
@@ -1,5 +1,7 @@
 import { act, render, screen } from "@testing-library/react";
+import { InvalidAuthException } from "../../services/auth/InvalidAuthException";
 import { NoAuthRedirectException } from "../../services/auth/NoAuthRedirectException";
+import { InternalErrorException } from "../../services/http/InternalErrorException";
 import { UnauthenticatedException } from "../../services/http/UnauthenticatedException";
 import { useRedirectAuth } from "./useRedirectAuth";
 
@@ -9,7 +11,7 @@ function TestUseRedirectAuth(props: { authService: any }) {
 }
 
 describe("useRedirectAuth hook", () => {
-  it("Should return a null user, null error and false loading if user is not authenticated and is not an auth redirect", async () => {
+  it("Should return a null user, null error and false loading if user is not authenticated and there was no auth redirect", async () => {
     const authService: any = {
       handleAuthRedirect: jest
         .fn()
@@ -27,6 +29,81 @@ describe("useRedirectAuth hook", () => {
     expect(container.innerHTML).toEqual(
       JSON.stringify({
         user: null,
+        error: null,
+        loading: false,
+      }),
+    );
+  });
+
+  it("Should return a null user, a non null error and false loading, if something went wrong during the auth redirect handler", async () => {
+    const user: any = { user: true };
+
+    jest.spyOn(console, "error").mockImplementation();
+
+    const authService: any = {
+      handleAuthRedirect: jest
+        .fn()
+        .mockRejectedValue(new InvalidAuthException("Oh no!")),
+      getSignedInUser: jest.fn().mockResolvedValue(user),
+    };
+
+    await act(async () => {
+      render(<TestUseRedirectAuth authService={authService} />);
+    });
+
+    const container = await screen.findByTestId("test");
+    expect(container.innerHTML).toEqual(
+      JSON.stringify({
+        user: null,
+        error: new Error(
+          "An error occurred while signing you in. Please try again.",
+        ),
+        loading: false,
+      }),
+    );
+  });
+
+  it("Should return a null user, a non null error, and false loading if auth redirect was successful but fetching signed in user failed", async () => {
+    jest.spyOn(console, "error").mockImplementation();
+
+    const authService: any = {
+      handleAuthRedirect: jest.fn().mockResolvedValue(undefined),
+      getSignedInUser: jest
+        .fn()
+        .mockRejectedValue(new InternalErrorException()),
+    };
+
+    await act(async () => {
+      render(<TestUseRedirectAuth authService={authService} />);
+    });
+
+    const container = await screen.findByTestId("test");
+    expect(container.innerHTML).toEqual(
+      JSON.stringify({
+        user: null,
+        error: new Error(
+          "An error occurred while signing you in. Please try again.",
+        ),
+        loading: false,
+      }),
+    );
+  });
+
+  it("Should return a user, a null error, and false loading if auth redirect was successful and fetching signed in user succeeded", async () => {
+    const user: any = { test: true };
+    const authService: any = {
+      handleAuthRedirect: jest.fn().mockResolvedValue(undefined),
+      getSignedInUser: jest.fn().mockResolvedValue(user),
+    };
+
+    await act(async () => {
+      render(<TestUseRedirectAuth authService={authService} />);
+    });
+
+    const container = await screen.findByTestId("test");
+    expect(container.innerHTML).toEqual(
+      JSON.stringify({
+        user,
         error: null,
         loading: false,
       }),
