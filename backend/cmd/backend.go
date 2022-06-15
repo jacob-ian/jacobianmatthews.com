3f4f7083853062e2da37d7b33be6f828e0b37356
Jacob Ian Matthews <jacob@jacobian.com.au>
2022-06-15 10:42:29 +0800

    Setup simple HTTP go server

--- backend/cmd/backend.go
+++ backend/cmd/backend.go
@@ -1,10 +1,51 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
 )
 
+type Response struct {
+	Message string `json:"message"`
+}
+
+var Port int
+
 func main() {
-	fmt.Println("Hello there! It is " + time.Now().String())
+	Port := getPort()
+	mux := http.NewServeMux()
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			http.NotFound(w, r)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(Response{Message: "Success"})
+	})
+	mux.Handle("/", loggerMiddleware(handler))
+	log.Printf("Listening on Port %v\n", Port)
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(Port), mux))
+}
+
+func getPort() int {
+	portEnv, exists := os.LookupEnv("PORT")
+	if !exists {
+		log.Fatalln("Missing PORT environment variable")
+	}
+	port, err := strconv.ParseUint(portEnv, 10, 16)
+	if err != nil {
+		log.Fatalln("Invalid PORT environment variable")
+	}
+	return int(port)
+}
+
+func loggerMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%v %v %v", r.Method, r.URL.Path, r.UserAgent())
+		next.ServeHTTP(w, r)
+	})
 }
