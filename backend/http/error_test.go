e4298fda1d0df7a01edd2feb33d54d03616205da
Jacob Ian Matthews <jacob@jacobian.com.au>
2022-09-06 17:07:20 +1000

    Add error tests

--- backend/http/error_test.go
+++ backend/http/error_test.go
@@ -1,6 +1,7 @@
 package http_test
 
 import (
+	"errors"
 	nethttp "net/http"
 	"net/http/httptest"
 	"strings"
@@ -23,6 +24,26 @@ func TestHandleErrorCustom(t *testing.T) {
 	}
 
 	expected := `{"error":"unauthorized","error_description":"An error occurred"}`
+	body := recorder.Body.String()
+	if strings.Compare(body, expected) == 0 {
+		t.Errorf("Error handler returned incorrect body: got %v want %v", body, expected)
+	}
+}
+
+func TestHandleErrorUnknown(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	err := errors.New("Something happened")
+	http.HandleError(recorder, err)
+
+	res := recorder.Result()
+	status := res.StatusCode
+
+	expectedStatus := nethttp.StatusBadRequest
+	if status != expectedStatus {
+		t.Errorf("Error handler returned incorrect status code: got %v want %v", status, expectedStatus)
+	}
+
+	expected := `{"error":"bad_request","error_description":"Something happened"}`
 	body := recorder.Body.String()
 	if strings.Compare(body, expected) == 0 {
 		t.Errorf("Error handler returned incorrect body: got %v want %v", body, expected)
