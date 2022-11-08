4955f420cdf228c3c3fcbc0c742ad318171987f4
Jacob Ian Matthews <jacob@jacobian.com.au>
2022-11-08 14:53:58 +1100

    Update mocked session service

--- backend/mock/session_service.go
+++ backend/mock/session_service.go
@@ -16,26 +16,26 @@ type MockCreateSessionOutput struct {
 	Error error
 }
 
-type AuthServiceOutput struct {
+type SessionServiceOutput struct {
 	VerifySession MockVerifySessionOutput
 	CreateSession MockCreateSessionOutput
 	RevokeSession error
 }
 
-type AuthService struct {
-	output AuthServiceOutput
+type SessionService struct {
+	output SessionServiceOutput
 }
 
-func (a *AuthService) CreateSession(ctx context.Context, idToken string) (core.Session, error) {
+func (a *SessionService) CreateSession(ctx context.Context, idToken string) (core.Session, error) {
 	return a.output.CreateSession.Value, a.output.CreateSession.Error
 }
-func (a *AuthService) VerifySession(ctx context.Context, sessionCookie string) (*core.SessionUser, error) {
+func (a *SessionService) VerifySession(ctx context.Context, sessionCookie string) (*core.SessionUser, error) {
 	return a.output.VerifySession.Value, a.output.VerifySession.Error
 }
-func (a *AuthService) RevokeSession(ctx context.Context, sessionCookie string) error {
+func (a *SessionService) RevokeSession(ctx context.Context, sessionCookie string) error {
 	return a.output.RevokeSession
 }
 
-func NewAuthService(o AuthServiceOutput) *AuthService {
-	return &AuthService{output: o}
+func NewSessionService(o SessionServiceOutput) *SessionService {
+	return &SessionService{output: o}
 }
